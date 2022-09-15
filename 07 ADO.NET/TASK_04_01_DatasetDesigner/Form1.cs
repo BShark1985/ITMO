86a9a75c0a66090fc26fd27b08029790f0e03e23
Andrei Leontev <leonand1985@gmail.com>
2022-09-15 23:19:36 +0300

    task4.1

---
+++ 07 ADO.NET/TASK_04_01_DatasetDesigner/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TASK_04_DatasetDesigner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GetCustomersButton_Click(object sender, EventArgs e)  // Кнопка <Get Customers>
+        {
+            NorthwindDataSet NorthwindDataset1 = new NorthwindDataSet();
+            NorthwindDataSetTableAdapters.CustomersTableAdapter CustomersTableAdapter1 = new NorthwindDataSetTableAdapters.CustomersTableAdapter();
+            CustomersTableAdapter1.Fill(NorthwindDataset1.Customers);
+            foreach (NorthwindDataSet.CustomersRow NWCustomer in NorthwindDataset1.Customers.Rows)
+            {
+                CustomersListBox.Items.Add(NWCustomer.CompanyName);
+            }
+
+        }
+    }
+}
