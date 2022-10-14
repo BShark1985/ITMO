2b8cac155d3137b8c129a42bfa47f9026aea7995
Andrei Leontev <103438317+BShark1985@users.noreply.github.com>
2022-10-14 17:24:22 +0300

    Add files via upload

---
+++ 08 ASP.NET/ASP.NET.ExamStudentsAPP/ITMO.124-16.Leontev.ASP.NET.ExamStudentsAPP/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ITMO._124_16.Leontev.ASP.NET.ExamStudentsAPP
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
